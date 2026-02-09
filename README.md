95a9123934e8d5b9d51a75d319d55226aee9bd20
michael roorkee <michaeljohnkinyunyu@gmail.com>
2026-02-09 14:42:34 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+### my first project on crud app using crud operation
+
+run
+
+'''uvicorn app.main:app --reload'''
---
+++ app/schemas.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+class ItemBase(BaseModel):
+    title: str
+    description: str
+class ItemCreate(ItemBase):
+    pass 
+
+class Item(ItemBase):
+    id: int
+
+    class config:
+        orm_mode= True
